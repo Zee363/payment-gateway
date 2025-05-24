21fe2952440b1954ed37c752293bdcf4ee7b5e36
Zinhle Nhlapo <zinhle.nhlapo31@gmail.com>
2025-05-24 09:24:33 +0200

    Added a pipleline

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 const paymentRoutes = require('./routes/paymentRoutes');
 
 const PORT = process.env.PORT || 5000;
 
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 50, 
+    message: 'Too many requests from this IP, please try again later.',
+});
+
 app.use(cors())
 app.use(express.json());
+app.use(limiter);
 app.use('/api/v1/payments', paymentRoutes);
 app.get('/api/v1/payments/:reference', paymentRoutes)
 
